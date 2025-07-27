7b30c718ef2ae674707bee19e23ec5b2dbea1b35
AshwinderPalSingh <Asingh47_be23@thapar.edu>
2025-07-27 09:34:28 +0530

    skills list

---
+++ skills_list.py
@@ -0,0 +1,21 @@
+skills_list = [
+    # Programming Languages
+    "Python", "Java", "C++", "C#", "JavaScript", "TypeScript", "Ruby", "PHP", "Go", "Rust",
+    "Swift", "Kotlin", "R", "Scala", "Perl", "MATLAB", "SQL", "HTML", "CSS", "Shell",
+
+    # Software and Tools
+    "Creo", "AutoCAD", "SolidWorks", "Revit", "ANSYS", "MATLAB", "Eclipse", "Visual Studio",
+    "IntelliJ", "PyCharm", "Git", "Docker", "Kubernetes", "Jenkins", "Maven", "Gradle",
+    "Photoshop", "Illustrator", "Adobe Premiere", "Blender", "Unity", "Unreal Engine",
+
+    # Frameworks and Libraries
+    "React", "Angular", "Vue.js", "Django", "Flask", "Spring", "Node.js", "TensorFlow",
+    "PyTorch", "Keras", "Pandas", "NumPy", "Scikit-learn", ".NET", "Bootstrap",
+
+    # Databases
+    "MySQL", "PostgreSQL", "MongoDB", "Oracle", "SQLite", "Redis", "Cassandra",
+
+    # Other Skills
+    "Machine Learning", "Data Science", "DevOps", "Cloud Computing", "AWS", "Azure",
+    "Google Cloud", "Cybersecurity", "Network Administration", "UI/UX Design"
+]
